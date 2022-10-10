be63a837b15912490abdf61015338573d806b9d5
dsakariya <103394345+dsakariya@users.noreply.github.com>
2022-10-10 16:31:53 -0400

    Testing

--- SmartRecipe/ViewController.swift
+++ SmartRecipe/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func DidTapbutton(){
+        let vc = UIViewController()
+        vc.view.backgroundColor  = .red
+        navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
 

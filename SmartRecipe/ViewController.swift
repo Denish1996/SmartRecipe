996df4f3f7b09b412ac8806bc3ecd5fcceb844e0
Denish <kakadiyadenish@gmail.com>
2022-10-10 20:09:22 -0400

    API Response

--- SmartRecipe/ViewController.swift
+++ SmartRecipe/ViewController.swift
@@ -12,6 +12,43 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        struct RecipeResponse: Decodable{
+                    let hits : [Hits]
+                }
+                struct Hits:Decodable{
+                    let recipe : Recipe
+                }
+                struct Recipe : Decodable{
+                    let label : String
+                    let uri: String
+                    let ingredientLines :[String]
+                }
+        //        struct Ingredients: Decodable{
+        //            let ingredient: String
+        //        }
+                let url = URL(string: "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=907d2b4b&app_key=db6697b492e4a4b02a8f3ea2b44abb79")
+                let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+                    if error != nil {
+                        print(error!)
+                    }
+                    else{
+                        if let data = data {
+                            do {
+        //                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                                let result = try! JSONDecoder().decode(RecipeResponse.self, from: data)
+                                print(result.hits[0].recipe.label)
+                                print(result.hits[0].recipe.uri)
+                                print(result.hits[0].recipe.ingredientLines)
+        //                        print(jsonResult)
+        //                        print(jsonResult["hits"]!!)
+                            }
+                            catch{
+                                print("JSon Parsing error")
+                            }
+                        }
+                    }
+                }
+                task.resume()
     }
     @IBAction func DidTapbutton(){
         let vc = UIViewController()
